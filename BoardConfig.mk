39bb1526786707333c58cc31766d68ccec4bce71
Joshua Lynch <jl52741@gmail.com>
2015-12-21 12:16:34 -0600

    angler: TWRP brinup
    kang'd from TeamWin

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,4 +128,14 @@ WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
 -include vendor/huawei/angler/BoardConfigVendor.mk
